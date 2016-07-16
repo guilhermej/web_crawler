6aaa0894a6f98ae05493ec4a74ca79022df66310
Guilherme Junqueira <guilhermebjs@gmail.com>
2016-07-16 10:58:27 -0300

    Add files via upload

---
+++ web_crawler.py
@@ -0,0 +1,34 @@
+#####################################
+# Python para Pentesters            #
+# https://solyd.com.br/treinamentos #
+#####################################
+
+import requests
+import re
+
+to_crawl = ['http://uol.com.br']
+crawled = set()
+
+header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                        'AppleWebKit/537.36 (KHTML, like Gecko) '
+                        'Chrome/51.0.2704.103 Safari/537.36'}
+
+while True:
+    url = to_crawl[0]
+    try:
+        req = requests.get(url, headers=header)
+    except:
+        to_crawl.remove(url)
+        crawled.add(url)
+        continue
+
+    html = req.text
+    links = re.findall(r'<a href="?\'?(https?:\/\/[^"\'>]*)', html)
+    print 'Crawling:', url
+
+    to_crawl.remove(url)
+    crawled.add(url)
+
+    for link in links:
+        if link not in crawled and link not in to_crawl:
+            to_crawl.append(link)
